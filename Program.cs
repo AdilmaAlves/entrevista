79e0fabdeaf3c83b18bf1e3eb87bc2f44ff02252
Adilma Alves <125414087+AdilmaAlves@users.noreply.github.com>
2023-02-14 21:05:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿String nome,sobrenome,cor;
+int idade;
+
+Console.WriteLine("Olá, Tudo bem? Qual o seu nome?");
+ nome =Console.ReadLine();
+Console.WriteLine("Qual o seu sobrenome?");
+ sobrenome =Console.ReadLine();
+Console.WriteLine("Qual a sua idade?");
+ idade =int.Parse(Console.ReadLine());
+Console.WriteLine("Qual a sua cor favorita?");
+ cor =Console.ReadLine();
+
+
+Console.WriteLine($"Nome: {nome} \nSobrenome: {sobrenome} \nIdade: {idade} \nCor Favorita: {cor}" );
+
+
